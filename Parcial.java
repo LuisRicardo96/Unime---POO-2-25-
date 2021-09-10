b9d072168b158a1160d904d607fac9e32c475be5
Ricardo Borges <luisr6864@gmail.com>
2021-09-10 16:56:16 -0300

    ATIVIDADE PARCIAL POO 2 ( 25% )
    Programa que ler as notas do aluno e tira a media.

---
+++ Parcial.java
@@ -0,0 +1,41 @@
+/*
+* <UNIME>
+* <BSI>
+* <PROGAMAÇÃO ORIENTADA A OBJETO 2>
+* <PABLO ROXO>
+* <LUIS RICARDO SOUSA BORGES>
+*/
+
+
+
+package parcial1;
+import java.util.Scanner;
+
+public class Parcial {
+
+	public static void main(String[] args) {
+		double nota1, nota2, nota3, resultado;
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite a 1° nota: ");
+		nota1 =  teclado.nextDouble();
+		
+		System.out.println("Digite a 2° nota: ");
+		nota2 =  teclado.nextDouble();
+		
+		System.out.println("Digite a 3° nota: ");
+		nota3 =  teclado.nextDouble();
+		
+		System.out.println("A nota final é: ");
+		System.out.println(resultado = ((nota1 + nota2 + nota3) / 3));
+		
+		if (resultado >= 7) {
+			System.out.println("Você esta aprovado");
+		}
+		 else if (resultado <= 4) {
+				System.out.println("Você esta reprovado");
+		 }
+		
+	}
+
+}
